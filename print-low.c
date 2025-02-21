8f85bda4605dccf9be42cdcd15aad6e447282d23
adamstepansky <adamstepansky@brandeis.edu>
2025-02-21 15:16:22 -0500

    Create print-low.c

---
+++ print-low.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("Enter decimal numbers");
+        return 1;
+    }
+
+    for (int i = 1; i < argc; i++) {
+        int number = strtol(argv[i], NULL, 10);
+        int low_8_bits = number & 0xFF;
+        printf("%-3d 0x%02X %3d\n", i, low_8_bits, low_8_bits);
+    }
+
+    return 0;
+}
