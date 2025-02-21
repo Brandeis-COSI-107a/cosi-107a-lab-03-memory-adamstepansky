57825ee62bd7d21661a03df748a252b8009fccad
adamstepansky <adamstepansky@brandeis.edu>
2025-02-21 15:43:16 -0500

    Create print-byte1.c

---
+++ print-byte1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("Not enough values");
+        return 1;
+    }
+
+    for (int i = 1; i < argc; i++) {
+        unsigned long num;
+
+        if (strncmp(argv[i], "0x", 2) == 0) {
+            num = strtol(argv[i], NULL, 16);
+        } else {
+            num = strtol(argv[i], NULL, 10);
+        }
+
+        unsigned char byte1 = (num >> 8) & 0xFF;
+
+        printf("%d 0x%02lX %d\n", i, byte1, byte1);
+    }
+
+    return 0;
+}
