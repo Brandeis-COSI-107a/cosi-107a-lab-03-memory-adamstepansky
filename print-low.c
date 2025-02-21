0e38c302f320488b8ee892d1bdfd8e482d53350e
adamstepansky <adamstepansky@brandeis.edu>
2025-02-21 15:37:13 -0500

    Update print-low.c

--- print-low.c
+++ print-low.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
-        printf("Enter decimal numbers");
+        printf("Not enough values");
         return 1;
     }
 
     for (int i = 1; i < argc; i++) {
-        int number = strtol(argv[i], NULL, 10);
+        int num;
+        
+        if (strncmp(argv[i], "0x", 2) == 0) {
+            num = strtol(argv[i], NULL, 16);
+        } else {
+            num = strtol(argv[i], NULL, 10);
+        }
+        
         int low_8_bits = number & 0xFF;
         printf("%d 0x%02X %3d\n", i, low_8_bits, low_8_bits);
     }
